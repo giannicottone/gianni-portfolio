8164934d8bad9610bf52f4a1ad81583765b01666
Gianni Cottone <gjcottone@outlook.com>
2025-08-27 12:40:40 -0400

    Added Base Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="mt-16 border-t">
+      <div className="mx-auto">
+        © {new Date().getFullYear()} Gianni Cottone
+      </div>
+    </footer>
+  );
+}
