d50e361a80c0f372eb4e841eff6f0c904180d7ee
Gianni Cottone <gjcottone@outlook.com>
2025-12-01 10:40:41 -0500

    Fixed Error in devTest page.tsx

--- src/app/devtest/page.tsx
+++ src/app/devtest/page.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import Field from "@/components/forms/Field";
 
 export default function Page() {
   return(
       <main className="text-center text-xl font-medium">
-       <Field label="Name" name="name" autoComplete="name" />
       </main>
   )
 }
