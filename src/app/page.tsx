75ac7c2448605dcc8061dab476c10190ea69efcf
Gianni Cottone <gjcottone@outlook.com>
2025-12-08 17:26:36 -0500

    deleted junk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
       <div className = "flex flex-auto items-center justify-center">
+        
       <section className ="text-center">
         <p>
         Hi, I{"'"}m Gianni Cottone 
@@ -22,6 +23,7 @@ export default function Home() {
             Contact
           </Link></nav>
       </section>
+
       </div>
   );
 }
