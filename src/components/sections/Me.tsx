4757fb32a357fc320d6c40ef4f66f598bb507569
Gianni Cottone <79020439+giannicottone@users.noreply.github.com>
2026-03-31 19:38:31 -0400

    Update Me.tsx

--- src/components/sections/Me.tsx
+++ src/components/sections/Me.tsx
@@ -24,6 +24,18 @@ export default function Me() {
           {/* Identity */}
           <div className="flex flex-col">
             <SectionHeader underline title = "Hi, I'm Gianni"/>
+<div className="
+      bg-[rgba(var(--card),0.8)]
+      backdrop-blur-md
+      rounded-xl p-6
+
+      shadow-md
+      transition-all duration-200 ease-out
+
+      hover:-translate-y-1
+      hover:shadow-xl
+      hover:bg-[rgba(var(--card),0.65)]">
+      <div className="grid md:grid-cols-2 gap-6">
             <p className="text-body">
               Based in New York. I graduated with a B.S in Computer Science from University at Buffalo in Spring of 2025 with a focus on software development. I&apos;m excited to learn new tools and work on industry solutions.
             </p>
@@ -38,6 +50,7 @@ export default function Me() {
               Trying New Foods · Horror Movies · Cycling · Escape Rooms · Fighting Games
             </p>
           </div>
+</div>
           <div className="flex flex-row gap-4">
           <LinkButton label="View Resume" href="/Gianni_Cottone_Resume.pdf"/>
           <LinkButton label = "Linkedin" href = "https://linkedin.com/in/gianni-cottone" />
