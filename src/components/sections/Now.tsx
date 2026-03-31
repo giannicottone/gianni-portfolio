f170ab531880171e81db97f07be6bbd5d6c29da2
Gianni Cottone <79020439+giannicottone@users.noreply.github.com>
2026-03-31 19:39:04 -0400

    Update Now.tsx

--- src/components/sections/Now.tsx
+++ src/components/sections/Now.tsx
@@ -5,6 +5,18 @@ export default function Now() {
   return (
     <ComponentWrapper>
       <SectionHeader title="Right Now" />
+<div className="
+      bg-[rgba(var(--card),0.8)]
+      backdrop-blur-md
+      rounded-xl p-6
+
+      shadow-md
+      transition-all duration-200 ease-out
+
+      hover:-translate-y-1
+      hover:shadow-xl
+      hover:bg-[rgba(var(--card),0.65)]">
+      <div className="grid md:grid-cols-2 gap-6">
       <ul className="text-body list-disc pl-5 flex flex-col gap-2">
       <li>
         Learning new technologies with a focus on underlying systems and behavior
@@ -16,6 +28,7 @@ export default function Now() {
         Iterating on this site to better reflect how I think and build as an engineer
       </li>
     </ul>
+</div>
     </ComponentWrapper>
   );
 }
